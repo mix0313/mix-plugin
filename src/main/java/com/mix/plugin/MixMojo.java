efeb58313fb2d15f929ecce438ac24f0ea9fcb2f
709648421@qq.com <oschina>
2018-08-26 22:35:03 +0800

    插件简单实例

---
+++ src/main/java/com/mix/plugin/MixMojo.java
@@ -0,0 +1,19 @@
+package com.mix.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * @author: mix
+ * @Date: 2018/8/26
+ */
+@Mojo(name = "mix", defaultPhase = LifecyclePhase.TEST)
+public class MixMojo extends AbstractMojo {
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        System.out.println("插件测试");
+    }
+}
