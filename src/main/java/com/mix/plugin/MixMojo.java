3d16a80c006299d04f0a95e3a1ffeb648a369c5b
mix <709648421@qq.com>
2018-11-05 18:22:10 +0800

    插件简单实例

--- src/main/java/com/mix/plugin/MixMojo.java
+++ src/main/java/com/mix/plugin/MixMojo.java
@@ -5,6 +5,9 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.util.List;
 
 /**
  * @author: mix
@@ -12,10 +15,14 @@ import org.apache.maven.plugins.annotations.Mojo;
  */
 @Mojo(name = "mix", defaultPhase = LifecyclePhase.TEST)
 public class MixMojo extends AbstractMojo {
+    @Parameter(defaultValue = "test")
+    private String msg;
+    @Parameter
+    private List<String> options;
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         System.out.println("插件测试");
-
-        //test1233232
+        System.out.println(msg);
+        System.out.println(options.toString());
     }
 }
